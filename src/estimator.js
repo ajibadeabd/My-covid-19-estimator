7f0f8f79d5576f142644b7c9d2fe51fd329ba661
ajibadeabd <ajibadeabd@gmail.com>
2020-04-11 23:26:42 +0100

    1-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,75 +1,75 @@
-// test('adds properly', () => {
-//   expect(currentlyInfected(1, 5)).toBe(5);
-// });
-
-
-const estimator = {
-  currentlyInfected: (reportedCases, number, periodType) => {
-    let cas;
-    if (periodType === 'months') {
-      cas = reportedCases * number * 30;
-    }
-    if (periodType === 'days') {
-      cas = reportedCases * number;
-    }
-    if (periodType === 'weeks') {
-      cas = reportedCases * number * 7;
-    }
-    return cas;
-  },
-  infectionsByRequstedTime: (reportedCases, number, timeToElapse, periodType) => {
-    let power;
-    let answer;
-    if (periodType === 'months') {
-      power = 2 ** Math.floor((timeToElapse * 30) / 3);
-      answer = estimator.currentlyInfected(reportedCases, number, periodType) * power;
-    }
-    if (periodType === 'weeks') {
-      power = 2 ** Math.floor((timeToElapse * 7) / 3);
-      answer = estimator.currentlyInfected(reportedCases, number, periodType) * power;
-    }
-    if (periodType === 'days') {
-      power = 2 ** Math.floor(timeToElapse / 3);
-      answer = estimator.currentlyInfected(reportedCases, number, periodType) * power;
-    }
-    return answer;
-  },
-  severeCasesByRequestedTime: (reportedCases, number, timeToElapse, periodType) => {
-    const tim = estimator.infectionsByRequstedTime(reportedCases, number, timeToElapse, periodType);
-    return tim * (15 / 100);
-  },
-  hospitalBedsByRequestedTime: (reportedCases,
-    number, timeToElapse, periodType, totalHospitalBeds) => {
-    let final;
-    const TP = totalHospitalBeds * (35 / 100);
-    const tim = estimator.severeCasesByRequestedTime(reportedCases, number, timeToElapse,
-      periodType);
-    const answer = TP - tim;
-    if (answer < 0) {
-      final = -Math.floor(Math.abs(answer));
-    } if (answer > 0) {
-      final = answer;
-    }
-    return final;
-  },
-
-  casesForICUByRequestedTime: (reportedCases, number, timeToElapse, periodType) => {
-    const iB = estimator.infectionsByRequstedTime(reportedCases, number, timeToElapse, periodType);
-    return Math.floor(iB * (5 / 100));
-  },
-  casesForVentilatorsByRequestedTime: (reportedCases, number, timeToElapse, periodType) => {
-    const iBR = estimator.infectionsByRequstedTime(reportedCases, number, timeToElapse, periodType);
-    return Math.floor(iBR * (2 / 100));
-  },
-  dollarInFlight: (avgDailyIncomeInUSD, reportedCases, number, timeToElapse, periodType) => {
-    const iBR = estimator.infectionsByRequstedTime(reportedCases, number, timeToElapse, periodType);
-    const dIF = (iBR * avgDailyIncomeInUSD * 0.73);
-    return Math.floor(dIF);
-  },
-
-};
-
 const covid19ImpactEstimator = () => {
+  const estimator = {
+    currentlyInfected: (reportedCases, number, periodType) => {
+      let cas;
+      if (periodType === 'months') {
+        cas = reportedCases * number * 30;
+      }
+      if (periodType === 'days') {
+        cas = reportedCases * number;
+      }
+      if (periodType === 'weeks') {
+        cas = reportedCases * number * 7;
+      }
+      return cas;
+    },
+    infectionsByRequstedTime: (reportedCases, number, timeToElapse, periodType) => {
+      let power;
+      let answer;
+      if (periodType === 'months') {
+        power = 2 ** Math.floor((timeToElapse * 30) / 3);
+        answer = estimator.currentlyInfected(reportedCases, number, periodType) * power;
+      }
+      if (periodType === 'weeks') {
+        power = 2 ** Math.floor((timeToElapse * 7) / 3);
+        answer = estimator.currentlyInfected(reportedCases, number, periodType) * power;
+      }
+      if (periodType === 'days') {
+        power = 2 ** Math.floor(timeToElapse / 3);
+        answer = estimator.currentlyInfected(reportedCases, number, periodType) * power;
+      }
+      return answer;
+    },
+    severeCasesByRequestedTime: (reportedCases, number, timeToElapse, periodType) => {
+      const tim = estimator.infectionsByRequstedTime(reportedCases, number, timeToElapse,
+        periodType);
+      return tim * (15 / 100);
+    },
+    hospitalBedsByRequestedTime: (reportedCases,
+      number, timeToElapse, periodType, totalHospitalBeds) => {
+      let final;
+      const TP = totalHospitalBeds * (35 / 100);
+      const tim = estimator.severeCasesByRequestedTime(reportedCases, number, timeToElapse,
+        periodType);
+      const answer = TP - tim;
+      if (answer < 0) {
+        final = -Math.floor(Math.abs(answer));
+      } if (answer > 0) {
+        final = answer;
+      }
+      return final;
+    },
+
+    casesForICUByRequestedTime: (reportedCases, number, timeToElapse, periodType) => {
+      const iB = estimator.infectionsByRequstedTime(reportedCases, number, timeToElapse,
+        periodType);
+      return Math.floor(iB * (5 / 100));
+    },
+    casesForVentilatorsByRequestedTime: (reportedCases, number, timeToElapse, periodType) => {
+      const iBR = estimator.infectionsByRequstedTime(reportedCases, number, timeToElapse,
+        periodType);
+      return Math.floor(iBR * (2 / 100));
+    },
+    dollarInFlight: (avgDailyIncomeInUSD, reportedCases, number, timeToElapse, periodType,
+      avgDailyIncomepopulation) => {
+      const iBR = estimator.infectionsByRequstedTime(reportedCases, number, timeToElapse,
+        periodType, timeToElapse);
+      const dIF = (iBR * avgDailyIncomeInUSD * avgDailyIncomepopulation) / timeToElapse;
+      return Math.floor(dIF);
+    },
+
+  };
+
   const input = {
     region: {
       name: 'Africa',
@@ -103,7 +103,8 @@ const covid19ImpactEstimator = () => {
         estimator.casesForVentilatorsByRequestedTime(input.reportedCases,
           10, input.timeToElapse, input.periodType),
       dollarInFlight: estimator.dollarInFlight(input.region.avgDailyIncomeInUSD,
-        input.reportedCases, 10, input.timeToElapse, input.periodType),
+        input.reportedCases, 10, input.timeToElapse, input.periodType,
+        input.region.avgDailyIncomepopulation, input.timeToElapse),
 
     },
     severeImpact: {
@@ -125,7 +126,8 @@ const covid19ImpactEstimator = () => {
        estimator.casesForVentilatorsByRequestedTime(input.reportedCases,
          50, input.timeToElapse, input.periodType),
       dollarInFlight: estimator.dollarInFlight(input.region.avgDailyIncomeInUSD,
-        input.reportedCases, 50, input.timeToElapse, input.periodType),
+        input.reportedCases, 50, input.timeToElapse, input.periodType,
+        input.region.avgDailyIncomepopulation, input.timeToElapse),
     },
   };
   return estimates;
